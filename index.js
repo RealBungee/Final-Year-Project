eee3f722bed264e5753bb0876370e8aae2a5a204
bungee <nikola.banek.kc2@gmail.com>
2022-01-21 18:29:49 +0000

    Reworked timeline retrieval function to get basic timeline  retrieval functionality working

--- index.js
+++ index.js
@@ -39,63 +39,63 @@ client.on('ready', () => {
 
     // we request the author_id expansion so that we can print out the user name later
     let params = {
-        "max_id": since,
-        "tweet.fields": "created_at",
-        "expansions": "author_id"
-    }
+      "max_results": 100,
+      "tweet.fields": "created_at",
+      "expansions": "author_id"
+  }
 
-    const options = {
-        headers: {
-            "User-Agent": "v2UserTweetsJS",
-            "authorization": `Bearer ${twitter.bearerToken}`
-        }
-    }
+  const options = {
+      headers: {
+          "User-Agent": "v2UserTweetsJS",
+          "authorization": `Bearer ${twitter.bearerToken}`
+      }
+  }
 
-    let hasNextPage = true;
-    let nextToken = null;
-    let userName;
-    console.log("Retrieving Tweets...");
+  let hasNextPage = true;
+  let nextToken = null;
+  let userName;
+  console.log("Retrieving Tweets...");
 
-    while (hasNextPage) {
-        let resp = await getPage(params, options, nextToken);
-        if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
-            userName = resp.includes.users[0].username;
-            if (resp.data) {
-                userTweets.push.apply(userTweets, resp.data);
-            }
-            if (resp.meta.next_token) {
-                nextToken = resp.meta.next_token;
-            } else {
-                hasNextPage = false;
-            }
-        } else {
-            hasNextPage = false;
-        }
-    }
+  while (hasNextPage) {
+      let resp = await getPage(params, options, nextToken);
+      if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+          userName = resp.includes.users[0].username;
+          if (resp.data) {
+              userTweets.push.apply(userTweets, resp.data);
+          }
+          if (resp.meta.next_token) {
+              nextToken = resp.meta.next_token;
+          } else {
+              hasNextPage = false;
+          }
+      } else {
+          hasNextPage = false;
+      }
+  }
 
-    console.dir(userTweets, {
-        depth: null
-    });
-    console.log(`Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`);
-    return userTweets;
+  console.dir(userTweets, {
+      depth: null
+  });
+  console.log(`Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`);
+
 }
 
 const getPage = async (params, options, nextToken) => {
-    if (nextToken) {
-        params.pagination_token = nextToken;
-    }
+  if (nextToken) {
+      params.pagination_token = nextToken;
+  }
 
-    try {
-        const resp = await needle('get', url, params, options);
+  try {
+      const resp = await needle('get', url, params, options);
 
-        if (resp.statusCode != 200) {
-            console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
-            return;
-        }
-        return resp.body;
-    } catch (err) {
-        throw new Error(`Request failed: ${err}`);
-    }
+      if (resp.statusCode != 200) {
+          console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
+          return;
+      }
+      return resp.body;
+  } catch (err) {
+      throw new Error(`Request failed: ${err}`);
+  }
 }
 
 client.login(token);
