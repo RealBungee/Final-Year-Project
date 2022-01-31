4b7f0fe8ec1709399c5e30d69320516b72eb5b10
bungee <nikola.banek.kc2@gmail.com>
2022-01-31 13:28:12 +0000

    removed merge conflict text

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //discord imports
 const fs = require('fs');
 const { token, twitter } = require('./config.json');
@@ -14,23 +13,6 @@ for(const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
-=======
-//imports
-const fs = require('fs');
-const needle = require('needle');
-const { Client, Intents, Message } = require('discord.js');
-const { token, clientId, guildId, twitter } = require('./config.json');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], partials: ["MESSAGE", "CHANNEL", "REACTION"] });
-
-client.commands = new Collection();
-
-const since = 1453839051379724289;
-
-const prefix = '!';
-
-// const channel = client.channels.fetch('934165450084978718')
-//   .then(channel => console.log(channel.name));
->>>>>>> 89896bf97b2c5a3272ae4064b08f11d51de2c29e
 
 //Actions to perform when Bot comes online
 client.on('ready', async () => {
@@ -38,7 +20,6 @@ client.on('ready', async () => {
   
   const registrationChannel = await client.channels.fetch('934165450084978718')
   .catch(console.error);
-
   const registrationMessage = await registrationChannel.messages.fetch('934166359552688159')
   .catch(console.error);
   console.log(registrationMessage.content);
@@ -55,7 +36,6 @@ client.on('interactionCreate', async interaction => {
 
   const command = client.commands.get(interaction.commandName);
 
-<<<<<<< HEAD
   if(!command) return;
 
   try{
@@ -63,17 +43,6 @@ client.on('interactionCreate', async interaction => {
   } catch (error){
       console.error(error);
       return interaction.reply({ content: 'There was an error executing this command!', ephemeral: true});
-=======
-  if (commandName === 'ping') {
-    client.get('reactionrole').execute(message, args, Discord, client);
-  } else if (commandName === 'server') {
-    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-  } else if (commandName === 'user') {
-    getUserTweets();
-    await interaction.reply('Elon Musk tweeted about DOGE in tweet: https://twitter.com/elonmusk/status/1454876031232380928 \n Bought DOGE with 15 minute hold period');
-  } else if (commandName === 'settradeholdtime'){
-    await interaction.reply('Trade hold time set to 15 minutes');
->>>>>>> 89896bf97b2c5a3272ae4064b08f11d51de2c29e
   }
 });
 
@@ -95,73 +64,4 @@ client.on('interactionCreate', async interaction => {
 //   console.log(`Collected ${collected.size} items`);
 // });
 
-<<<<<<< HEAD
-=======
-  const userId = "44196397";
-  const url = `https://api.twitter.com/2/users/${userId}/tweets`;
-  const getUserTweets = async () => {
-    let userTweets = [];
-
-    // we request the author_id expansion so that we can print out the user name later
-    let params = {
-      "max_results": 100,
-      "tweet.fields": "created_at",
-      "expansions": "author_id"
-  }
-
-  const options = {
-      headers: {
-          "User-Agent": "v2UserTweetsJS",
-          "authorization": `Bearer ${twitter.bearerToken}`
-      }
-  }
-
-  let hasNextPage = true;
-  let nextToken = null;
-  let userName;
-  console.log("Retrieving Tweets...");
-
-  while (hasNextPage) {
-      let resp = await getPage(params, options, nextToken);
-      if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
-          userName = resp.includes.users[0].username;
-          if (resp.data) {
-              userTweets.push.apply(userTweets, resp.data);
-          }
-          if (resp.meta.next_token) {
-              nextToken = resp.meta.next_token;
-          } else {
-              hasNextPage = false;
-          }
-      } else {
-          hasNextPage = false;
-      }
-  }
-
-  console.dir(userTweets, {
-      depth: null
-  });
-  console.log(`Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`);
-
-}
-
-const getPage = async (params, options, nextToken) => {
-  if (nextToken) {
-      params.pagination_token = nextToken;
-  }
-
-  try {
-      const resp = await needle('get', url, params, options);
-
-      if (resp.statusCode != 200) {
-          console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
-          return;
-      }
-      return resp.body;
-  } catch (err) {
-      throw new Error(`Request failed: ${err}`);
-  }
-}
-
->>>>>>> 89896bf97b2c5a3272ae4064b08f11d51de2c29e
 client.login(token);
