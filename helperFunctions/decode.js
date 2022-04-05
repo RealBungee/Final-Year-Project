ef1cfde7ef9647d07a5687e17faed46e7b55f9d3
bungee <nikola.banek.kc2@gmail.com>
2022-04-05 12:36:19 +0100

    Added encrypt/decrypt functionality for saving API's to the database

--- helperFunctions/decode.js
+++
@@ -1,3 +0,0 @@
-//planned to create a decoding algorithm for retrieveing
-//encoded user data from a database and decoding it
-//Data like: Exchange API_KEY and API_SECRET
--- helperFunctions/encode.js
+++
@@ -1,3 +0,0 @@
-//planned to create an encoding algorithm for saving
-//sensitive user data to a database
-//Data like: Exchange API_KEY and API_SECRET
